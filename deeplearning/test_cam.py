6bde078d129fd114875f38589e1b3cbc6aeb0cd4
doorunderground <wlgk55311@gmail.com>
2026-02-07 22:19:56 +0900

    0207

--- deeplearning/test_cam.py
+++ deeplearning/test_cam.py
@@ -1,7 +1,11 @@
 import cv2
 import Face_detect
+import numpy as np
 
 cam = cv2.VideoCapture(0)
+
+
+num = 0
 
 while True:
     ret, image = cam.read()
@@ -12,10 +16,23 @@ while True:
     rects, shapes = Face_detect.detect(image)
     dst = image.copy()
     Face_detect.draw(dst, rects, shapes)
-
-
+    
     cv2.imshow("dst", dst)
     key = cv2.waitKey(1)
+    
     if key == ord('q'):
         print('Quit')
         break
+    
+    if len(shapes) == 0:
+        continue
+    
+    if key == ord('s'):
+        arr = Face_detect.to_numpy(rects[0], shapes[0])
+        np.save(f'C:\\study\\deeplearning\\data\\s_doorunderground_{num}.npy', arr)
+        
+    if key == ord('w'):
+        arr = Face_detect.to_numpy(rects[0], shapes[0])
+        np.save(f'C:\\study\\deeplearning\\data\\s_doorunderground_{num}.npy', arr)
+
+    num += 1
